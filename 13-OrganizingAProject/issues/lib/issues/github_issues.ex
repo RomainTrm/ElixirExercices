05eba0b0781963f18b9c8ad9c800eaba479aa9c7
DESKTOP-5V2OR16\Romain <you@example.com>
2020-01-07 10:01:53 +0100

    Organizing a project, step 4

--- 13-OrganizingAProject/issues/lib/issues/github_issues.ex
+++ 13-OrganizingAProject/issues/lib/issues/github_issues.ex
@@ -1,9 +1,12 @@
 defmodule Issues.GithubIssues do
+  require Logger
+
   @user_agent [ { "User_agent", "Elixir dave@pragprog.com" } ]
   @github_url Application.get_env(:issues, :github_url)
 
-
   def fetch(user, project) do
+    Logger.info("Fetching #{user}'s project #{project}")
+
     issues_url(user, project)
     |> HTTPoison.get(@user_agent)
     |> decode_response
@@ -14,6 +17,9 @@ defmodule Issues.GithubIssues do
   end
 
   def decode_response({_, %{status_code: status_code, body: body}}) do
+    Logger.info("Got response: status code=#{status_code}")
+    Logger.debug(fn -> inspect(body) end)
+
     {
       status_code |> check_for_error,
       body |> Poison.Parser.parse!(%{})
